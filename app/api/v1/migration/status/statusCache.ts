33c52b7a68d0c01abcfc03d68fca82bacf618365
Azure Production <production@sentinel-mediator.com>
2026-08-07 05:32:01 +0000

    fix(streamer): define ID_REGEX and enable crontab @reboot auto-restart for 24/7 persistence

--- app/api/v1/migration/status/statusCache.ts
+++ app/api/v1/migration/status/statusCache.ts
@@ -1,6 +1,6 @@
 export const LIVE_CACHE = {
-  timestamp: "2026-08-05T13:11:54Z",
-  ak3_blobs: 12887,
+  timestamp: "2026-08-07T05:32:01Z",
+  ak3_blobs: 13305,
   ak4_blobs: 2139,
   ak5_blobs: 10242
 }
