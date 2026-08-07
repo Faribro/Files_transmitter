d124e2caf8727d0b3f6c1fdd980c0b5c44c5983b
Azure Production <production@sentinel-mediator.com>
2026-08-07 11:09:19 +0000

    fix(akross): force red live progress card for July 2026 in DriveExplorer component

--- app/api/v1/migration/status/statusCache.ts
+++ app/api/v1/migration/status/statusCache.ts
@@ -1,8 +1,8 @@
 export const LIVE_CACHE = {
-  timestamp: "2026-08-07T11:06:55Z",
+  timestamp: "2026-08-07T11:09:14Z",
   ak3_blobs: 52276,
   ak4_blobs: 21148,
   ak5_blobs: 10242,
-  ak6_blobs: 7177,
+  ak6_blobs: 7444,
   ak7_blobs: 6922
 }
