45ea46ae004cf0c6f824c8d8618ece1567ab5709
Azure Production <production@sentinel-mediator.com>
2026-08-07 08:12:16 +0000

    Auto-update live migration status: Apr=11,114

--- app/api/v1/migration/status/statusCache.ts
+++ app/api/v1/migration/status/statusCache.ts
@@ -1,6 +1,6 @@
 export const LIVE_CACHE = {
-  timestamp: "2026-08-07T08:11:43Z",
+  timestamp: "2026-08-07T08:12:16Z",
   ak3_blobs: 52276,
-  ak4_blobs: 7219,
+  ak4_blobs: 11114,
   ak5_blobs: 10242
 }
