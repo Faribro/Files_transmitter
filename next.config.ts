ab860378d11580e1613f751aa69519ed3aa5ee6c
Azure Production <production@sentinel-mediator.com>
2026-08-10 06:50:17 +0000

    fix(deploy): fix Next 16 Turbopack build error on Vercel by enabling webpack explicitly in vercel.json & next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   allowedDevOrigins: ['medical-transmitter.duckdns.org', 'tb-engine.allianceindia.org'],
   async headers() {
     return [
